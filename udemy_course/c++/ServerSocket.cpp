0f882482021f7ff4bf3255c2ae81edd10d3d79b6
knapsack7 <knapsack120@gmail.com>
2025-06-10 23:49:41 +0530

    basic-121_socket_programming

---
+++ udemy_course/c++/ServerSocket.cpp
@@ -0,0 +1,82 @@
+/*
+ * Server Socket Program in C++
+ * This program creates a TCP server that listens for incoming connections
+ * and sends a message to any connected client.
+ */
+
+#include <sys/socket.h>    // For socket functions
+#include <netinet/in.h>    // For sockaddr_in structure
+#include <arpa/inet.h>     // For inet_ntoa function
+#include <stdio.h>         // For printf
+#include <stdlib.h>        // For exit
+#include <unistd.h>        // For close
+#include <errno.h>         // For error handling
+#include <string.h>        // For string operations
+#include <sys/types.h>     // For system types
+#include <iostream>        // For C++ I/O operations
+
+int main()
+{
+    // Socket file descriptors
+    int server = 0;    // File descriptor for server socket
+    int client = 0;    // File descriptor for client socket
+    
+    // Structure to hold server address information
+    struct sockaddr_in serverAddr;
+    
+    // Message to send to client
+    const char* message = "Hello from C++ server!";
+
+    // Create a TCP socket
+    // AF_INET: IPv4 protocol
+    // SOCK_STREAM: TCP socket type
+    // 0: Protocol (0 means default protocol for the given type)
+    // socket for listening incoming connections
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Configure server address structure
+    serverAddr.sin_addr.s_addr = INADDR_ANY;  // Accept connections from any IP
+    serverAddr.sin_family = AF_INET;          // IPv4 address family
+    serverAddr.sin_port = htons(5555);        // Port number (converted to network byte order)
+
+    // Bind the socket to the specified address and port
+    // This associates the socket with a specific network interface and port
+    bind(server, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    // Start listening for incoming connections
+    // 5: Maximum length of the queue of pending connections,
+    // if there would be more than 5 pending connections, the server will reject the 
+    // new connections
+    listen(server, 5);
+
+    std::cout << "Server is listening for connections..." << std::endl;
+
+    // Accept an incoming connection
+    // This blocks until a client connects
+    // NULL parameters mean we don't need client address information
+    client = accept(server, (struct sockaddr*)NULL, NULL);
+    std::cout << "Client connected!" << std::endl;
+
+    // Send message to client
+    // Parameters:
+    // - client: Client socket file descriptor
+    // - message: Data to send
+    // - strlen(message): Length of data
+    // - 0: Flags (0 means no special flags)
+    if(send(client, message, strlen(message), 0) != strlen(message))
+    {
+        std::cout << "Error sending message to client" << std::endl;
+    }
+    else
+    {
+        std::cout << "Message sent successfully" << std::endl;
+    }
+
+    // Close the client connection
+    close(client);
+    std::cout << "Client disconnected" << std::endl;
+
+    return 0;
+}
+
+
---
+++ udemy_course/c/ClientSocket.c
@@ -0,0 +1,63 @@
+/*
+ * Client Socket Program in C
+ * This program creates a TCP client that connects to a server
+ * and receives a message from it.
+ */
+
+#include <sys/socket.h>    // For socket functions
+#include <sys/types.h>     // For system types
+#include <netinet/in.h>    // For sockaddr_in structure
+#include <netdb.h>         // For network database operations
+#include <stdio.h>         // For printf
+#include <string.h>        // For string operations
+#include <stdlib.h>        // For exit
+#include <unistd.h>        // For close
+#include <errno.h>         // For error handling
+#include <arpa/inet.h>     // For inet_addr function
+
+int main()
+{
+    // Socket file descriptor for the client
+    int serverSock = 0;
+    
+    // Structure to hold server address information
+    struct sockaddr_in addr;
+
+    // Create a TCP socket
+    // AF_INET: IPv4 protocol
+    // SOCK_STREAM: TCP socket type
+    // 0: Protocol (0 means default protocol for the given type)
+    serverSock = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Configure server address structure
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Server IP (localhost)
+    addr.sin_family = AF_INET;                      // IPv4 address family
+    addr.sin_port = htons(5555);                    // Server port (converted to network byte order)
+
+    // Connect to the server
+    // This establishes a connection with the server
+    connect(serverSock, (struct sockaddr *)&addr, sizeof(addr));
+    printf("Connected to server! \n");
+
+    // Buffer to store received data
+    char buffer[1025];
+    int val;
+
+    // Read data from the server
+    // Parameters:
+    // - serverSock: Socket file descriptor
+    // - buffer: Buffer to store received data
+    // - 1024: Maximum number of bytes to read
+    if((val = read(serverSock, buffer,1024)) != 0)
+    {
+        // Null terminate the received data
+        buffer[val] = '\0';
+        printf("Read successful and buffer read is %s \n", buffer);
+    }
+
+    // Close the socket connection
+    close(serverSock);
+    printf("Socket closed.\n");
+}
+
+
